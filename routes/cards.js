a256a6e621ee0fec3e1d53f5337e23968f1913f2
Maksim Vasilev <luckjunk@yandex.ru>
2023-09-16 16:39:20 +0300

    tests2

--- routes/cards.js
+++ routes/cards.js
@@ -5,8 +5,8 @@ const cardsController = require('../controllers/cards');
 
 router.get('/', cardsController.getCards);
 router.post('/', cardsController.createCard);
-router.delete('/:cardId', cardsController.deleteCard);
 router.put('/:cardId/likes', cardsController.likeCard);
 router.delete('/:cardId/likes', cardsController.dislikeCard);
+router.delete('/:cardId', cardsController.deleteCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userController = require('../controllers/users');
 
-router.post('/users', userController.createUser);
-router.get('/users/:userID', userController.getUserById);
-router.get('/users', userController.getUsers);
+router.post('/', userController.createUser);
+router.get('/:userId', userController.getUserById);
+router.get('/', userController.getUsers);
 router.patch('/me', userController.updateProfile);
 router.patch('/me/avatar', userController.updateAvatar);
 
