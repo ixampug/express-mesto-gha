47a6783151fd77c9cc211ee18d6645536dde518f
Maksim Vasilev <luckjunk@yandex.ru>
2023-09-16 16:23:22 +0300

    running tests

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/users');
 router.post('/users', userController.createUser);
 router.get('/users/:userID', userController.getUserById);
 router.get('/users', userController.getUsers);
+router.patch('/me', userController.updateProfile);
+router.patch('/me/avatar', userController.updateAvatar);
 
 module.exports = router;
