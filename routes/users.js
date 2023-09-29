9c64230fcdf8904c2395e2fe138fd630a6e38b9a
Maksim Vasilev <luckjunk@yandex.ru>
2023-09-29 13:25:11 +0300

    tests5

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/users');
 
 router.post('/', userController.createUser);
-router.get('/:userId', userController.getUserById);
+router.get('/:userID', userController.getUserById);
 router.get('/', userController.getUsers);
 router.patch('/me', userController.updateProfile);
 router.patch('/me/avatar', userController.updateAvatar);
