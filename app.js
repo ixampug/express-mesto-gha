7c820255fb780ece014d50f09def849e66c1b183
Maksim Vasilev <luckjunk@yandex.ru>
2023-10-02 15:39:42 +0300

    addHelmet

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
+const helmet = require('helmet');
+
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
@@ -23,6 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
